55136b499a58170812c33361484679900fdd6bfd
maximilian <mengxinnian@gmail.com>
2021-08-24 11:08:06 +0800

    调整yml

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import './public-path';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.global.less';
+
+if (window.__POWERED_BY_QIANKUN__) {
+    // eslint-disable-next-line no-undef
+    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
 
 function render(props) {
     const { container } = props;
