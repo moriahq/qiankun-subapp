55c83897d5c92b9d508d4ec001a3802d1f29e00a
maximilian <mengxinnian@gmail.com>
2021-08-24 10:14:13 +0800

    Rate组件

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Input, Button } from '@osui/ui';
+import { Input, Button, Rate } from '@osui/ui';
 import Error from '../../statics/error.png';
 import Logo from '../../statics/logo.svg';
 import Logo2 from '../../statics/Gitee-Scan.svg';
@@ -17,6 +17,7 @@ const Home = () => {
       <h2>这是微前端页面~</h2>
       <Button type="primary" onClick={goDemoPage}>goDemoPage</Button>
       <Input placeholder="input 组件测试" />
+      <Rate allowHalf defaultValue={2.5} />
       <div>
         <img src={Error} />
       </div>
