73bb905e326c09ddfd0c821185f2c40aabb3511b
wusilun <wusilun@baidu.com>
2021-08-25 12:25:08 +0800

    chore:代码美化

--- webpack.config.js
+++ webpack.config.js
@@ -14,215 +14,225 @@ const distOutputPath = 'dist';
 
 // output配置
 const outputConfig = isProd =>
-(isProd
+  isProd
     ? {
         filename: 'js/[name].[chunkhash].min.js',
         path: path.resolve(__dirname, distOutputPath),
         publicPath: './',
         library: 'proxima-plugin',
         libraryTarget: 'umd',
-    }
+      }
     : {
         filename: 'main.js',
         path: path.resolve(__dirname, distOutputPath),
         publicPath: '/',
-    });
+      };
 
 const getLocalIdent = ({ resourcePath }, localIdentName, localName) => {
-    if (/\.global\.(css|less)$/.test(resourcePath) || /node_modules/.test(resourcePath)) {
-        // 不做cssModule 处理的
-        return localName;
-    }
-    return `${localName}__${hasha(resourcePath + localName, { algorithm: 'md5' }).slice(0, 8)}`;
+  if (/\.global\.(css|less)$/.test(resourcePath) || /node_modules/.test(resourcePath)) {
+    // 不做cssModule 处理的
+    return localName;
+  }
+  return `${localName}__${hasha(resourcePath + localName, { algorithm: 'md5' }).slice(0, 8)}`;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = (cliEnv = {}, argv) => {
-    const mode = argv.mode;
+  const mode = argv.mode;
 
-    if (!['production', 'development'].includes(mode)) {
-        throw new Error('The mode is required for NODE_ENV, BABEL_ENV but was not specified.');
-    }
+  if (!['production', 'development'].includes(mode)) {
+    throw new Error('The mode is required for NODE_ENV, BABEL_ENV but was not specified.');
+  }
 
-    const isProd = mode === 'production';
-    const isDev = mode === 'development';
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
 
-    const classNamesConfig = {
-        loader: '@ecomfe/class-names-loader',
-        options: {
-            classNamesModule: require.resolve('classnames'),
-        },
-    };
-    // 生产环境使用 MiniCssExtractPlugin
-    const extractOrStyleLoaderConfig = isProd ? MiniCssExtractPlugin.loader : 'style-loader';
+  const classNamesConfig = {
+    loader: '@ecomfe/class-names-loader',
+    options: {
+      classNamesModule: require.resolve('classnames'),
+    },
+  };
+  // 生产环境使用 MiniCssExtractPlugin
+  const extractOrStyleLoaderConfig = isProd ? MiniCssExtractPlugin.loader : 'style-loader';
 
-    // 根据 patterns 使用 style-resources-loader
-    const makeStyleResourcesLoader = patterns => ({
-        loader: 'style-resources-loader',
-        options: {
-            patterns,
-            injector: 'append',
-        },
-    });
+  // 根据 patterns 使用 style-resources-loader
+  const makeStyleResourcesLoader = patterns => ({
+    loader: 'style-resources-loader',
+    options: {
+      patterns,
+      injector: 'append',
+    },
+  });
 
-    const lessLoaderConfig = {
-        loader: 'less-loader',
-        options: {
-            lessOptions: {
-                javascriptEnabled: true,
-                modifyVars: {
-                    'ant-prefix': 'ant',
-                },
-                plugins: [new LessPluginFunctions({ alwaysOverride: true })],
-            },
+  const lessLoaderConfig = {
+    loader: 'less-loader',
+    options: {
+      lessOptions: {
+        javascriptEnabled: true,
+        modifyVars: {
+          'ant-prefix': 'ant',
         },
-    };
+        plugins: [new LessPluginFunctions({ alwaysOverride: true })],
+      },
+    },
+  };
 
-    const cssLoaderConfig = {
-        loader: 'css-loader',
-        options: {
-            modules: { getLocalIdent },
-            importLoaders: 1,
-        },
-    };
+  const cssLoaderConfig = {
+    loader: 'css-loader',
+    options: {
+      modules: { getLocalIdent },
+      importLoaders: 1,
+    },
+  };
 
-    const postcssLoaderConfig = {
-        loader: 'postcss-loader',
-        options: {
-            postcssOptions: {
-                plugins: [
-                    namespacePefixer({
-                        namespace: '.proxima-plugin',
-                    }),
-                ],
-            },
-        },
-    };
-    const webpackConfig = {
-        entry: './src/index.tsx',
-        mode: isProd ? 'production' : 'development',
-        output: outputConfig(isProd),
-        devtool: (() => {
-            if (isProd) {
-                return 'source-map';
-            }
-            if (isDev) {
-                return 'inline-cheap-module-source-map';
-            }
-            return false;
-        })(),
-        resolve: {
-            extensions: ['.js', '.css', '.jsx', '.tsx', '.ts'],
-            alias: {
-                '@': path.resolve(__dirname, 'src/')
-            }
-        },
-        devServer: {
-            hot: "only",
-            static: {
-                directory: path.resolve(__dirname, '../dist'),
-                serveIndex: true,
-                watch: true,
-            },
-            historyApiFallback: {
-                disableDotRule: true,
-                index: '/',
-            },
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Headers": "*",
-              "Access-Control-Allow-Methods": "*",
-            },
-        },
+  const postcssLoaderConfig = {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
         plugins: [
-            new WebpackBar(),
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, 'public/index.html'),
-                filename: 'index.html',
-                inject: true,
-            }),
-            isProd
-            && new MiniCssExtractPlugin({
-                filename: '[name].[contenthash].css',
-                chunkFilename: '[name].[contenthash].chunk.css',
-            }),
-            new CleanWebpackPlugin(),
-        ].filter(Boolean),
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env', '@babel/preset-react'],
-                            plugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
-                        },
-                    },
-                },
-                {
-                    test: /\.css/,
-                    include: [path.resolve(__dirname, 'src')],
-                    use: [classNamesConfig, extractOrStyleLoaderConfig, cssLoaderConfig],
-                },
-                {
-                    test: /\.css/,
-                    include: [
-                        path.resolve(__dirname, 'node_modules/antd/'),
-                        path.resolve(__dirname, 'node_modules/@osui'),
-                        path.resolve(__dirname, 'node_modules/github-markdown-css'),
-                    ],
-                    use: [classNamesConfig, extractOrStyleLoaderConfig, 'css-loader', postcssLoaderConfig],
-                },
-                {
-                    test: /\.less$/,
-                    // eslint-disable-next-line max-len
-                    include: [path.resolve(__dirname, 'node_modules/antd/'), path.resolve(__dirname, 'node_modules/@osui')],
-                    use: [
-                        extractOrStyleLoaderConfig,
-                        cssLoaderConfig,
-                        postcssLoaderConfig,
-                        lessLoaderConfig,
-                        makeStyleResourcesLoader([
-                            path.resolve(__dirname, 'node_modules/@osui/theme/dist/antd-vars-patch.less'),
-                            path.resolve(__dirname, 'node_modules/@osui/theme/dist/less-functions-overrides.less'),
-                        ]),
-                    ],
-                },
-                {
-                    test: /\.less$/,
-                    include: [path.resolve(__dirname, 'src')],
-                    use: [
-                        classNamesConfig,
-                        extractOrStyleLoaderConfig,
-                        cssLoaderConfig,
-                        'postcss-loader',
-                        lessLoaderConfig,
-                        makeStyleResourcesLoader([
-                            path.resolve(__dirname, 'node_modules/@osui/theme/dist/antd-vars-patch.less'),
-                            path.resolve(__dirname, 'node_modules/@osui/theme/dist/less-functions-overrides.less'),
-                        ]),
-                    ],
-                },
-                // 静态资源
-                {
-                    test: /\.(png|jpg|gif)$/i,
-                    type: 'asset/resource',
-                    generator: {
-                        filename: 'resource/[hash][ext][query]',
-                    },
-                },
-                {
-                    test: /\.svg$/,
-                    use: ['@svgr/webpack'],
-                },
-            ],
+          namespacePefixer({
+            namespace: '.proxima-plugin',
+          }),
+        ],
+      },
+    },
+  };
+  const webpackConfig = {
+    entry: './src/index.tsx',
+    mode: isProd ? 'production' : 'development',
+    output: outputConfig(isProd),
+    devtool: (() => {
+      if (isProd) {
+        return 'source-map';
+      }
+      if (isDev) {
+        return 'inline-cheap-module-source-map';
+      }
+      return false;
+    })(),
+    resolve: {
+      extensions: ['.js', '.css', '.jsx', '.tsx', '.ts'],
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+      },
+    },
+    devServer: {
+      hot: 'only',
+      static: {
+        directory: path.resolve(__dirname, '../dist'),
+        serveIndex: true,
+        watch: true,
+      },
+      historyApiFallback: {
+        disableDotRule: true,
+        index: '/',
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': '*',
+      },
+    },
+    plugins: [
+      new WebpackBar(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        filename: 'index.html',
+        inject: true,
+      }),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: '[name].[contenthash].css',
+          chunkFilename: '[name].[contenthash].chunk.css',
+        }),
+      new CleanWebpackPlugin(),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
         },
-    };
-    return isProd ? webpackConfig : smp.wrap(webpackConfig);
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+            },
+          },
+        },
+        {
+          test: /\.css/,
+          include: [path.resolve(__dirname, 'src')],
+          use: [classNamesConfig, extractOrStyleLoaderConfig, cssLoaderConfig],
+        },
+        {
+          test: /\.css/,
+          include: [
+            path.resolve(__dirname, 'node_modules/antd/'),
+            path.resolve(__dirname, 'node_modules/@osui'),
+            path.resolve(__dirname, 'node_modules/github-markdown-css'),
+          ],
+          use: [classNamesConfig, extractOrStyleLoaderConfig, 'css-loader', postcssLoaderConfig],
+        },
+        {
+          test: /\.less$/,
+          // eslint-disable-next-line max-len
+          include: [
+            path.resolve(__dirname, 'node_modules/antd/'),
+            path.resolve(__dirname, 'node_modules/@osui'),
+          ],
+          use: [
+            extractOrStyleLoaderConfig,
+            cssLoaderConfig,
+            postcssLoaderConfig,
+            lessLoaderConfig,
+            makeStyleResourcesLoader([
+              path.resolve(__dirname, 'node_modules/@osui/theme/dist/antd-vars-patch.less'),
+              path.resolve(
+                __dirname,
+                'node_modules/@osui/theme/dist/less-functions-overrides.less',
+              ),
+            ]),
+          ],
+        },
+        {
+          test: /\.less$/,
+          include: [path.resolve(__dirname, 'src')],
+          use: [
+            classNamesConfig,
+            extractOrStyleLoaderConfig,
+            cssLoaderConfig,
+            'postcss-loader',
+            lessLoaderConfig,
+            makeStyleResourcesLoader([
+              path.resolve(__dirname, 'node_modules/@osui/theme/dist/antd-vars-patch.less'),
+              path.resolve(
+                __dirname,
+                'node_modules/@osui/theme/dist/less-functions-overrides.less',
+              ),
+            ]),
+          ],
+        },
+        // 静态资源
+        {
+          test: /\.(png|jpg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'resource/[hash][ext][query]',
+          },
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
+      ],
+    },
+  };
+  return isProd ? webpackConfig : smp.wrap(webpackConfig);
 };
